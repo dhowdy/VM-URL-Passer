f5940a61106e240676965bfaa4a3b226ea99fc12
Don Howdeshell <dhowdeshell@gmail.com>
2016-05-23 11:29:01 -0500

    url_tcp_client.pl
    Client that runs on the VM.  Be sure to set this as the default program to open all HTTP and HTTPS links (instead of a browser installed on the guest VM).

---
+++ url_tcp_client.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use IO::Socket::INET;
+
+# auto-flush on socket
+$| = 1;
+
+# create a connecting socket
+my $socket = new IO::Socket::INET (
+    PeerHost => '192.168.100.100',#IP of host system
+    PeerPort => '7777',
+    Proto => 'tcp',
+);
+die "cannot connect to the server $!\n" unless $socket;
+print "connected to the server\n";
+
+# data to send to a server
+my $req = "$ARGV[0]";
+my $pass = "0o897348972394872394";
+my $data = "$pass\~\~$req";
+my $size = $socket->send($data);
+#print "sent data of length $size\n";
+
+# notify server that request has been sent
+shutdown($socket, 1);
+
+# receive a response of up to 1024 characters from server
+#my $response = "";
+#$socket->recv($response, 1024);
+#print "received response: $response\n";
+
+
+$socket->close();
